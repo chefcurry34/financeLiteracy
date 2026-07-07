2d1171c6277623eb747fa4d5cbc90d77a433f89d
chefcurry34 <gogo.vrbsky@gmail.com>
2026-07-07 18:47:58 +0200

    test git

--- src/main/java/com/gogdev/financeliteracy/FinanceLiteracyApplication.java
+++ src/main/java/com/gogdev/financeliteracy/FinanceLiteracyApplication.java
@@ -2,7 +2,7 @@ package com.gogdev.financeliteracy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//fuck me
 @SpringBootApplication
 public class FinanceLiteracyApplication {
 
