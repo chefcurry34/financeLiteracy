341ae57c24b7e979e712b4c0c8d2d67aaad41406
chefcurry34 <gogo.vrbsky@gmail.com>
2026-07-07 18:35:16 +0200

    Initial commit

---
+++ src/main/java/com/gogdev/financeliteracy/FinanceLiteracyApplication.java
@@ -0,0 +1,13 @@
+package com.gogdev.financeliteracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceLiteracyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceLiteracyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gogdev/financeliteracy/FinanceLiteracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogdev.financeliteracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceLiteracyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
